5fbb0d50f09f936b68bcf94e6cd2bf945c16ce3a
pythonajf <67696045+pythonajf@users.noreply.github.com>
2020-07-04 15:52:24 -0400

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,23 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+#Webhook into Git to get the latest code
+import git
+app = Flask(__name__)
+@app.route('/', methods=['POST'])
+    def webhook():
+        if request.method == 'POST':
+            repo = git.Repo('path/to/git_repo')
+            origin = repo.remotes.origin
+origin.pull()
+return 'Updated PythonAnywhere successfully', 200
+        else:
+            return 'Wrong event type', 400
+app = Flask(__name__)
+#End of webhook
+
+
+@app.route('/home')
+def hello_world():
+    return 'Hello from Flask!'
